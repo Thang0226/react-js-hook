fd1f51db2c5a0a5cce70180cf5ff870bbbc0e931
Thang0226 <thang.nd0226@gmail.com>
2025-01-22 14:31:02 +0700

    use custom hook: useClock.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Counter from "./components/Counter";
 import Selector from "./components/Selector";
+import MyClock from "./components/MyClock";
 
 function App() {
   return (
     // <Counter/>
-      <Selector/>
+    //   <Selector/>
+      <MyClock />
   );
 }
 
